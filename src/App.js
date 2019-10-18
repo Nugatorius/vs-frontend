ca1428288fb63c6e4272cc36d2a70641afe82229
Vivek <94.vivek.pawar@gmail.com>
2019-10-18 22:24:47 +0530

    Video stream tested

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <video style="height: 400px; width:600px;" id="videoPlayer" controls muted="muted" autoplay>
-        <source src="http://localhost:3000/video" type="video/mp4" />
+      <video id="videoPlayer" controls muted="muted">
+        <source src="../assets/sample.mp4" type="video/mp4" />
       </video>
     </div>
   );
